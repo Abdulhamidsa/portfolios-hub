58d01df0aec61bfefc8618cb8e4ca637ca9b97af
Aboood <abdu3793@stud.kea.dk>
2024-09-02 14:41:39 +0200

    Initialize Express.js server with ES Modules and ESLint

---
+++ react-app/backend/my-app/util/db.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+export const connect = (url = process.env.MONGO_CONNECTION_STRING) => {
+  return mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  });
+};
