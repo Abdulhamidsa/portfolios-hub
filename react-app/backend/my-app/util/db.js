18d4c6dcc0c424f897220a6d77496a5021824073
Aboood <abdu3793@stud.kea.dk>
2024-09-02 15:47:27 +0200

    add mongodb url

--- react-app/backend/my-app/util/db.js
+++ react-app/backend/my-app/util/db.js
@@ -1,6 +1,13 @@
 import mongoose from "mongoose";
-
-export const connect = (url = process.env.MONGO_CONNECTION_STRING) => {
+// export const connect = (url = process.env.MONGO_CONNECTION_STRING) => {
+//   return mongoose.connect(url, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true,
+//   });
+// };
+const MONGO_CONNECTION_STRING = "mongodb+srv://aboood:UNBFqjTpLgeUMQkl@cluster0.bn3dcrh.mongodb.net/internship?retryWrites=true&w=majority";
+export const connect = (url = MONGO_CONNECTION_STRING) => {
   return mongoose.connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
