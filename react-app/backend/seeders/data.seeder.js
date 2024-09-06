62658b4866552be0088f495a0330be3a7726c75d
Abdulhamidsa <email@example.com>
2024-09-06 15:11:40 +0200

    update: updating seeding file

--- react-app/backend/seeders/data.seeder.js
+++ react-app/backend/seeders/data.seeder.js
@@ -1,6 +1,8 @@
 import { User } from '../src/models/user.model.js'
 import { faker } from '@faker-js/faker'
 import { connect } from '../util/db.js'
+import { Projects } from '../src/models/projects.model.js'
+import mongoose from 'mongoose'
 const predefinedLinks = ['cv', 'social media', 'pdf cv']
 const predefinedProfessions = [
     'web development',
@@ -16,7 +18,6 @@ const predefinedProfessions = [
     'creative director',
     'digital marketer',
 ]
-
 const generateUserData = (count) => {
     return Array.from({ length: count }).map(() => ({
         credentials: {
@@ -48,15 +49,55 @@ const generateUserData = (count) => {
         updatedAt: faker.date.recent(),
     }))
 }
+const generateProjectsData = (count, userId) => {
+    return Array.from({ length: count }).map(() => ({
+        userId: new mongoose.Types.ObjectId(faker.helpers.arrayElement(userId)),
+        title: faker.lorem.words(3),
+        description: faker.lorem.sentences(2),
+        projectUrl: faker.internet.url(),
+        projectImage: Array.from({ length: 3 }).map(() => faker.image.url()),
+        projectThumbnail: faker.image.url(),
+    }))
+}
+const generateTagsData = (count) => {
+    return Array.from({ length: count }).map(() => ({
+        name: faker.lorem.word(),
+    }))
+}
 
+const deleteExistingData = async () => {
+    try {
+        await Promise.all([User.deleteMany({}), Projects.deleteMany({})])
+        console.log('data deleted')
+    } catch (error) {
+        console.error('failed deleting data:', error)
+        throw error
+    }
+}
+const insertData = async () => {
+    try {
+        const users = generateUserData(10)
+        await User.insertMany(users)
+        const allUsers = await User.find({}, '_id').exec()
+        const userId = allUsers.map((user) => user._id.toString())
+
+        const projects = generateProjectsData(20, userId)
+        await Projects.insertMany(projects)
+
+        console.log('data inserted')
+    } catch (error) {
+        console.error('failed to insert data:', error)
+        throw error
+    }
+}
 const seed = async () => {
     try {
         await connect()
-        await Promise.all([User.deleteMany({}), User.insertMany(generateUserData(10))])
-        console.log('true')
+        await Promise.all([deleteExistingData(), insertData()])
+        console.log('data seeded')
         process.exit(0)
-    } catch (e) {
-        console.error(e)
+    } catch (error) {
+        console.error('failed seeding data', error)
         process.exit(1)
     }
 }
