1ae86ae9d3a3aab228094bdf58ab441e29b03abe
Abdulhamidsa <email@example.com>
2024-09-17 16:05:26 +0200

    refactor: add zod validation to signin form  + update responses handling in signin hook

--- react-app/frontend/src/hooks/use-signin.ts
+++ react-app/frontend/src/hooks/use-signin.ts
@@ -4,15 +4,18 @@ import { fetcher } from "@/services/api";
 const signin = async (data: object) => {
   try {
     const response = await fetcher(endPoints.auth.signin, data);
-    console.log(response);
+
     if (response?.result) {
       return { result: true, message: response?.message || "Login successful!" };
     } else {
       return { result: false, message: response?.message || "Login failed. Please try again." };
     }
   } catch (error) {
-    console.error("Login error:", error);
-    return { result: false, message: "Login failed. Pleaaaaase try again." };
+    if (error) {
+      return { result: false, message: (error as Error).message || "Login failed. Please try again." };
+    } else {
+      return { result: false, message: "Login failed. Please try again." };
+    }
   }
 };
 
