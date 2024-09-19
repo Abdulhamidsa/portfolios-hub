7323415d12abfa2181dbe1067045f13e3d8da47a
Abdulhamidsa <email@example.com>
2024-09-19 16:14:42 +0200

    remove hardcoded connection url

--- react-app/backend/util/db.js
+++ react-app/backend/util/db.js
@@ -1,8 +1,5 @@
 import mongoose from 'mongoose'
-const MONGO_CONNECTION_STRING =
-    process.env.MONGO_CONNECTION_STRING ||
-    'mongodb+srv://aboood:UNBFqjTpLgeUMQkl@cluster0.bn3dcrh.mongodb.net/internship?retryWrites=true'
-export const connect = async (url = MONGO_CONNECTION_STRING) => {
+export const connect = async (url = process.env.MONGO_CONNECTION_STRING) => {
     try {
         await mongoose.connect(url)
         console.log('Connected to MongoDB')
