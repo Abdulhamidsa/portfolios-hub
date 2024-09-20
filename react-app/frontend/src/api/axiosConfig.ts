4e623104f142dc08b284c14049c0f61a222a745d
Abdulhamidsa <email@example.com>
2024-09-20 17:11:35 +0200

    refactor: include the token in the header upon axios calls + redirect to login if token is not valid

--- react-app/frontend/src/api/axiosConfig.ts
+++ react-app/frontend/src/api/axiosConfig.ts
@@ -2,35 +2,6 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const baseUrl = "http://localhost:4000";
-
-export const axiosInstance = axios.create({
-  baseURL: baseUrl,
-  timeout: 10000,
-  headers: {
-    "Content-Type": "application/json",
-  },
-  withCredentials: true,
-});
-axiosInstance.interceptors.request.use(
-  (config) => {
-    try {
-      const accessToken = Cookies.get("accessToken");
-      console.log("Retrieved accessToken from cookies:", accessToken);
-      if (accessToken) {
-        config.headers.Authorization = `Bearer ${accessToken}`;
-      } else {
-        console.warn("No access token found");
-      }
-    } catch (error) {
-      console.error("Error retrieving access token:", error);
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
-
 export const endPoints = {
   frontpage: {
     projects: baseUrl + "/project/projects_home",
@@ -53,3 +24,36 @@ export const endPoints = {
     getById: baseUrl + "/project/get",
   },
 } as const;
+
+export const axiosInstance = axios.create({
+  baseURL: baseUrl,
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${Cookies.get("accessToken")}`,
+  },
+  withCredentials: true,
+});
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        await axiosInstance.post("/auth/refresh-token");
+        const newAccessToken = Cookies.get("accessToken");
+        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+
+        return axiosInstance(originalRequest);
+      } catch (err) {
+        console.error("Failed to refresh access token:", err);
+        window.location.href = "/";
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+export default axiosInstance;
