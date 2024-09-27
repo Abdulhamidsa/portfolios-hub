e09fc2f1cef5722d92d9ce06328f4a808ec619f5
Abdulhamidsa <email@example.com>
2024-09-27 10:21:30 +0200

    refactor: fix import

--- react-app/backend/src/routes/user/route.get.user.js
+++ react-app/backend/src/routes/user/route.get.user.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { getUserProfile } from '../../handlers/user.controller.js'
+import { getUserProfile } from '../../handlers/user.handlers.js'
 import { checkAuthentication } from '../../../util/auth.js'
 const router = Router()
 router.get('/profile', checkAuthentication, getUserProfile)
