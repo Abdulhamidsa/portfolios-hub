038453b765a9bc4ffb9a986b835a09d8ddc53e11
Abdulhamidsa <email@example.com>
2024-09-27 16:18:07 +0200

    refactor: move endpoints to config folder

--- react-app/frontend/src/api/axiosConfig.ts
+++ react-app/frontend/src/api/axiosConfig.ts
@@ -1,29 +1,6 @@
+import { baseUrl } from "@/config/apiEndpoints";
 import axios from "axios";
 import Cookies from "js-cookie";
-
-const baseUrl = "http://localhost:4000";
-export const endPoints = {
-  frontpage: {
-    projects: baseUrl + "/project/projects_home",
-  },
-  auth: {
-    signin: baseUrl + "/user/signin",
-    register: baseUrl + "/user/register",
-    logout: baseUrl + "/user/logout",
-    refreshToken: baseUrl + "/user/refresh-token",
-  },
-  user: {
-    profile: baseUrl + "/user/profile",
-    updateProfile: baseUrl + "/user/update-profile",
-    changePassword: baseUrl + "/user/change-password",
-  },
-  project: {
-    create: baseUrl + "/project/create",
-    update: baseUrl + "/project/update",
-    delete: baseUrl + "/project/delete",
-    getById: baseUrl + "/project/get",
-  },
-} as const;
 
 export const axiosInstance = axios.create({
   baseURL: baseUrl,
@@ -34,7 +11,6 @@ export const axiosInstance = axios.create({
   },
   withCredentials: true,
 });
-
 axiosInstance.interceptors.response.use(
   (response) => response,
   async (error) => {
@@ -50,7 +26,7 @@ axiosInstance.interceptors.response.use(
         return axiosInstance(originalRequest);
       } catch (err) {
         console.error("Failed to refresh access token:", err);
-        window.location.href = "/";
+        window.location.href = "/auth";
       }
     }
     return Promise.reject(error);
---
+++ react-app/frontend/src/config/apiEndpoints.ts
@@ -0,0 +1,26 @@
+export const baseUrl = import.meta.env.VITE_API_BASE_URL;
+export const endPoints = {
+  frontpage: {
+    projects: baseUrl + "/projects/all",
+  },
+  auth: {
+    signin: baseUrl + "/auth/signin",
+    register: baseUrl + "/auth/register",
+    logout: baseUrl + "/auth/signout",
+    checkAuth: baseUrl + "/auth/check-auth",
+    refreshToken: baseUrl + "/auth/refresh-access-token",
+  },
+  user: {
+    profile: baseUrl + "/user/profile",
+    updateProfile: baseUrl + "/user/update-profile",
+    changePassword: baseUrl + "/user/change-password",
+    UserProjects: baseUrl + "/user/:friendlyId/projects/all",
+  },
+  project: {
+    create: baseUrl + "/project/create",
+    update: baseUrl + "/project/update",
+    delete: baseUrl + "/project/delete",
+    getById: baseUrl + "/project/get",
+  },
+} as const;
+// as const to tell typescript that this object is readonly and should not be modified
