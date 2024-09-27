d912853786c2353dcba3a3da94491ea8264a3493
Abdulhamidsa <email@example.com>
2024-09-27 16:22:22 +0200

    refactor : fix exporting objects

--- react-app/frontend/src/services/api.tsx
+++ react-app/frontend/src/services/api.tsx
@@ -1,4 +1,4 @@
-import axiosInstance from "../api/axiosConfig";
+import { axiosInstance } from "../api/axiosConfig";
 
 export type ApiResponse<T> = {
   data: T;
@@ -6,7 +6,7 @@ export type ApiResponse<T> = {
   message: string;
 };
 
-async function getData<T>(path: string): Promise<T> {
+export async function getData<T>(path: string): Promise<T> {
   try {
     const response = await axiosInstance.get<T>(path);
     return response.data;
@@ -14,16 +14,12 @@ async function getData<T>(path: string): Promise<T> {
     throw new Error(`Failed to fetch data: ${(error as Error).message}`);
   }
 }
-
-const fetcher = async (url: string, data: object): Promise<ApiResponse<object>> => {
+export const fetcher = async (url: string, data: object): Promise<ApiResponse<object>> => {
   try {
     const response = await axiosInstance.post(url, data);
     return response.data;
-    console.log(response.data);
   } catch (error) {
     console.error("Fetch error:", error);
     throw error;
   }
 };
-
-export { getData, fetcher };
