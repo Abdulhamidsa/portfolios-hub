ae23587197ef241efbe49621a2d5bba2f573ca14
Abdulhamidsa <email@example.com>
2024-09-27 10:19:02 +0200

    refactor: delete unused files

--- react-app/backend/src/routes/project/project.routes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express'
-import getProjects from '../project.api.js'
-import { requiresLogin } from '../../../util/auth.js'
-const router = Router()
-
-router.get('/projects_home', requiresLogin, getProjects)
-export default router
--- react-app/backend/src/routes/user/user.auth.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import { signup, signin } from '../../../util/auth.js'
-import { Credential } from '../../models/credential.model.js'
-import { setModel } from '../../../middleware/setModel.js'
-
-const router = Router()
-router.post('/register', setModel(Credential), signup)
-router.post('/signin', setModel(Credential), signin)
-export default router
