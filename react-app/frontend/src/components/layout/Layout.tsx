3e8ef6cf2cb84e18c675ad03b54648a7fe9dbd8c
Aboood <abdu3793@stud.kea.dk>
2024-10-11 00:43:24 +0200

    clean: delete unused code

--- react-app/frontend/src/components/layout/Layout.tsx
+++ react-app/frontend/src/components/layout/Layout.tsx
@@ -1,20 +1,20 @@
-import { Navbar } from "./Navbar";
-import { ReactNode } from "react";
+// import { Navbar } from "../../pages/userApp/components/Navbar";
+// import { ReactNode } from "react";
 
-interface LayoutProps {
-  children: ReactNode;
-}
+// interface LayoutProps {
+//   children: ReactNode;
+// }
 
-const Layout: React.FC<LayoutProps> = ({ children }) => {
-  return (
-    <div className=" flex flex-col gap-10">
-      <header>
-        <Navbar />
-      </header>
-      <main>{children}</main>
-      <footer></footer>
-    </div>
-  );
-};
+// const Layout: React.FC<LayoutProps> = ({ children }) => {
+//   return (
+//     <div className=" flex flex-col gap-10">
+//       <header>
+//         <Navbar />
+//       </header>
+//       <main>{children}</main>
+//       <footer></footer>
+//     </div>
+//   );
+// };
 
-export default Layout;
+// export default Layout;
