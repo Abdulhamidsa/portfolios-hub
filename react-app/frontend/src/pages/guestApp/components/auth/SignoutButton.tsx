e7a6df75b0d2efa80b699a65c605dae9cbec5964
Aboood <abdu3793@stud.kea.dk>
2024-10-11 00:41:51 +0200

    refactor: improve forms design

---
+++ react-app/frontend/src/pages/guestApp/components/auth/SignoutButton.tsx
@@ -0,0 +1,21 @@
+import { Button } from "@/components/ui/button";
+import { useSignOut } from "@/hooks/useAuth";
+import { LogOut } from "lucide-react";
+
+export default function SignOutButton() {
+  const signOut = useSignOut();
+
+  const handleSignOut = async () => {
+    try {
+      await signOut();
+    } catch (error) {
+      console.error("Sign out failed:", error);
+    }
+  };
+
+  return (
+    <Button variant="outline" onClick={handleSignOut} className=" w-full">
+      <LogOut className=" h-4" />
+    </Button>
+  );
+}
