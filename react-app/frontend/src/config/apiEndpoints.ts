600cc75515b77765846ea45758963b9202f526fe
Abdulhamidsa <email@example.com>
2024-10-11 16:42:31 +0200

    refactor: fix paths and clean unused code

--- react-app/frontend/src/config/apiEndpoints.ts
+++ react-app/frontend/src/config/apiEndpoints.ts
@@ -1,26 +1,37 @@
 export const baseUrl = import.meta.env.VITE_API_BASE_URL;
 export const endPoints = {
   frontpage: {
-    projects: baseUrl + "/projects/all",
+    projects: `${baseUrl}/projects/all`,
   },
-  auth: {
-    signin: baseUrl + "/auth/signin",
-    register: baseUrl + "/auth/register",
-    logout: baseUrl + "/auth/signout",
-    checkAuth: baseUrl + "/auth/check-auth",
-    refreshToken: baseUrl + "/auth/refresh-access-token",
-  },
+
   user: {
-    profile: baseUrl + "/user/profile",
-    updateProfile: baseUrl + "/user/update-profile",
-    changePassword: baseUrl + "/user/change-password",
-    UserProjects: baseUrl + "/user/:friendlyId/projects/all",
+    auth: {
+      signin: `${baseUrl}/auth/signin`,
+      register: `${baseUrl}/auth/register`,
+      logout: `${baseUrl}/auth/signout`,
+      checkAuth: `${baseUrl}/auth/check-auth`,
+      refreshToken: `${baseUrl}/auth/refresh-tokens`,
+    },
+    public: {
+      projects: `${baseUrl}/user/projects/all`,
+    },
+    private: {
+      profile: `${baseUrl}/user/profile`,
+      updateProfile: `${baseUrl}/user/update-profile`,
+      changePassword: `${baseUrl}/user/change-password`,
+      UserProjects: `${baseUrl}/user/:friendlyId/projects/all`,
+    },
   },
   project: {
-    create: baseUrl + "/project/create",
-    update: baseUrl + "/project/update",
-    delete: baseUrl + "/project/delete",
-    getById: baseUrl + "/project/get",
+    public: {
+      get: `${baseUrl}/projects/all`,
+    },
+    private: {
+      create: `${baseUrl}/project/create`,
+      update: `${baseUrl}/project/update`,
+      delete: `${baseUrl}/project/delete`,
+      getById: `${baseUrl}/000000/projects/all`,
+    },
   },
 } as const;
 // as const to tell typescript that this object is readonly and should not be modified
