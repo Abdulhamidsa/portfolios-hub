2cd835c301d0cac20e5163fdab4ffbfa3a7b9f9c
Abdulhamidsa <email@example.com>
2024-10-11 10:06:02 +0200

    refactor: move out auth middleware + move generate tokens to the handler

--- react-app/backend/services/auth.service.js
+++ react-app/backend/services/auth.service.js
@@ -1,35 +1,31 @@
-import bcrypt from 'bcryptjs'
-import { generateNewToken, generateRefreshToken } from '../util/jwt.js'
+// import bcrypt  from 'bcryptjs'
+import { generateAccessToken, generateRefreshToken } from '../util/jwt.js'
 import { Credential } from '../src/models/credential.model.js'
 import { User } from '../src/models/user.model.js'
-import { verifyToken } from '../util/jwt.js'
-
+// import { verifyToken } from '../util/jwt.js'
+import AppError from '../util/error.handler.js'
 // signin service
-export const signinService = async (req, res) => {
-    const { email, password } = req.body
-    if (!email || !password) {
-        throw { message: 'email and password are required', status: 400 }
+export const signinService = async (data) => {
+    const { email, password } = data
+    const userCredential = await Credential.findOne({ email })
+    if (!userCredential) {
+        throw new AppError('Email not registered', 400)
     }
-    try {
-        const user = await Credential.findOne({ email })
-        if (!user) {
-            throw { message: 'Email not registered', status: 400 }
-        }
-        // const isPasswordValid = await bcrypt.compare(password, user.password)
-        if (password !== user.password) {
-            throw { message: 'Invalid email or password', status: 400 }
-        }
-        const accessToken = generateNewToken(user)
-        const refreshToken = generateRefreshToken(user)
-        res.cookie('accessToken', accessToken, { httpOnly: true, secure: true, path: '/' })
-        res.cookie('refreshToken', refreshToken, { httpOnly: true, secure: true, path: '/' })
-
-        return { accessToken }
-    } catch (error) {
-        throw { message: error.message || 'An error occurred while signing in', status: 500 }
+    if (password !== userCredential.password) {
+        throw new AppError('Invalid password', 400)
     }
+    const user = await User.findById(userCredential._id)
+    if (!user) {
+        throw new AppError('User not found', 404)
+    }
+    const payload = {
+        id: user._id,
+        userType: user.userType,
+        friendlyId: user.friendlyId,
+    }
+    const [accessToken, refreshToken] = await Promise.all([generateAccessToken(payload), generateRefreshToken(payload)])
+    return { accessToken, refreshToken }
 }
-
 // signout service
 export const signoutService = async (req) => {
     try {
@@ -46,30 +42,5 @@ export const signoutService = async (req) => {
         return { message: 'Signout successful' }
     } catch (error) {
         throw { message: error.message || 'An error occurred while signing out', status: 500 }
-    }
-}
-
-// required  login service
-export const checkAuthenticationService = async (req) => {
-    const authHeader = req.headers.authorization
-    if (!authHeader?.startsWith('Bearer ')) {
-        throw { message: 'Not authorized', status: 401 }
-    }
-
-    const token = authHeader.split(' ')[1]
-    try {
-        const payload = await verifyToken(token)
-        console.log('payload', payload)
-        const userCredential = await Credential.findById(payload.id).select('-password').lean().exec()
-        if (!userCredential) {
-            throw { message: 'User not found', status: 404 }
-        }
-        const user = await User.findOne({ _id: userCredential.userId }).select('friendlyId').lean().exec()
-        if (!user) {
-            throw { message: 'User profile not found', status: 404 }
-        }
-        req.user = { ...userCredential, friendlyId: user.friendlyId }
-    } catch (error) {
-        throw { message: error.message || 'An error occurred while authenticating user', status: 500 }
     }
 }
