6503a8ec51358082a84f322ed7e934cdadd1e029
Abdulhamidsa <email@example.com>
2024-10-11 16:45:30 +0200

    refactor: remove styling

--- react-app/frontend/src/components/layout/Layout.tsx
+++ react-app/frontend/src/components/layout/Layout.tsx
@@ -7,7 +7,7 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className=" flex flex-col gap-10">
+    <div className="">
       <header>
         <Navbar />
       </header>
