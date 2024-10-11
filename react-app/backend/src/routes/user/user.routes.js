6c25d84ad689425c97af63c1f7b273f4c5100c6b
Abdulhamidsa <email@example.com>
2024-10-11 10:22:20 +0200

    style: change file naming

---
+++ react-app/backend/src/routes/user/user.routes.js
@@ -0,0 +1,6 @@
+import { Router } from 'express'
+import { getUserProfile } from '../../handlers/user.handlers.js'
+// import { checkAuthentication } from '../../../middleware/authMiddleware.js'
+const router = Router()
+router.get('/profile', getUserProfile)
+export default router
