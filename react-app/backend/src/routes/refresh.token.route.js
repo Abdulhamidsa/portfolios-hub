1a2bd54e1f20f3f37aa9c930a18ef5701c25347e
Abdulhamidsa <email@example.com>
2024-10-11 09:58:49 +0200

    refactor: change file names

--- react-app/backend/src/routes/refresh.token.route.js
+++
@@ -1,6 +0,0 @@
-import { Router } from 'express'
-import { refreshAccessToeken } from 'util/refresh.token'
-const router = Router()
-
-router.get('/refres-token', refreshAccessToeken)
-export default router
--- react-app/backend/src/routes/user/route.get.user.js
+++
@@ -1,6 +0,0 @@
-import { Router } from 'express'
-import { getUserProfile } from '../../handlers/user.handlers.js'
-import { checkAuthentication } from '../../../middleware/authMiddleware.js'
-const router = Router()
-router.get('/profile', checkAuthentication, getUserProfile)
-export default router
