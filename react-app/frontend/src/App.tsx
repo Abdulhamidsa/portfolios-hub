8f9d14b17333111e8ff7b0802d2d0438c59e4c34
Abdulhamidsa <email@example.com>
2024-10-11 16:32:32 +0200

    feat: add auth provider with new routes system - two apps

--- react-app/frontend/src/App.tsx
+++ react-app/frontend/src/App.tsx
@@ -1,31 +1,37 @@
 import "@/App.css";
-import Loading from "@/components/loaders/Loading";
-import { routes } from "@/routes/routes";
+import Loading from "@/components/feedback/Loading";
+import { AuthProvider } from "@/context/authContext";
+import AppRoutes from "@/routes/AppRoutes";
 import { Suspense } from "react";
-import { Routes, Route } from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <Suspense fallback={<Loading />}>
-      <Routes>
-        {routes.map(({ path, element }) => (
-          <Route key={path} path={path} element={element} />
-        ))}
-      </Routes>
-    </Suspense>
+    <AuthProvider>
+      <AppRoutes />
+    </AuthProvider>
   );
 }
 
-export default App;
+// import "@/App.css";
+// import { useAuth } from "./api/AuthContext";
+// import ProtectedApp from "./apps/ProtectedApp";
+// import PublicApp from "./apps/PublicApp";
+// import Loading from "@/components/feedback/Loading";
+// import Layout from "@/components/layout/Layout";
 
-// const Wrapper = () => {
-//   const [isLogedIn, setIsLogedIn] = useState(false);
-//   useEffect(() => {
-//     setIsLogedIn(localStorage.getItem("Token") ? true : false);
-//   });
-//   return (
-//     <div className="App">
-//       {isLogedIn ? <App /> : <Login />}
-//     </div>
+// function App() {
+//   const { isLoggedIn, isLoading } = useAuth();
+//   if (isLoading) return <Loading />;
+
+//   return isLoggedIn ? (
+//     <Layout>
+//       <ProtectedApp />
+//     </Layout>
+//   ) : (
+//     <Layout>
+//       <PublicApp />
+//     </Layout>
 //   );
-// };
+// }
+
+// export default App;
