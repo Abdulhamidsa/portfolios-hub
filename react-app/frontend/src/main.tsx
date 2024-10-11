df5fc95eea914773bd3769806eb443e07bf30635
Abdulhamidsa <email@example.com>
2024-10-11 16:33:03 +0200

    refactor: remove global layout from the app

--- react-app/frontend/src/main.tsx
+++ react-app/frontend/src/main.tsx
@@ -1,15 +1,10 @@
+import "./index.css";
 import App from "./App.tsx";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import Layout from "./components/layout/Layout.tsx";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
-  <>
-    <BrowserRouter>
-      <Layout>
-        <App />
-      </Layout>
-    </BrowserRouter>
-  </>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
