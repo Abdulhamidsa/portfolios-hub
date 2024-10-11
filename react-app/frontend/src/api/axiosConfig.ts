bbcea5d03eb4e26a0355d8ae7f7d34c903f36215
Abdulhamidsa <email@example.com>
2024-10-11 16:33:56 +0200

    refactor: remove refreshtoken from axios interceptors

--- react-app/frontend/src/api/axiosConfig.ts
+++ react-app/frontend/src/api/axiosConfig.ts
@@ -2,34 +2,36 @@ import { baseUrl } from "@/config/apiEndpoints";
 import axios from "axios";
 import Cookies from "js-cookie";
 
-export const axiosInstance = axios.create({
+const axiosInstance = axios.create({
   baseURL: baseUrl,
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
-    Authorization: `Bearer ${Cookies.get("accessToken")}`,
   },
   withCredentials: true,
 });
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = Cookies.get("accessToken");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 axiosInstance.interceptors.response.use(
   (response) => response,
   async (error) => {
     const originalRequest = error.config;
-    if (error.response.status === 401 && !originalRequest._retry) {
+    if (error.response?.status === 401 && !originalRequest._retry) {
       originalRequest._retry = true;
-
-      try {
-        await axiosInstance.post("/auth/refresh-token");
-        const newAccessToken = Cookies.get("accessToken");
-        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
-
-        return axiosInstance(originalRequest);
-      } catch (err) {
-        console.error("Failed to refresh access token:", err);
-        window.location.href = "/auth";
-      }
+      return axiosInstance(originalRequest);
     }
+    window.location.href = "/guest";
+    localStorage.clear();
     return Promise.reject(error);
   }
 );
+
 export default axiosInstance;
