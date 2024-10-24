06c533caede5fdcb5aa570a34f60a6e42c0d04f3
Aboood <abdu3793@stud.kea.dk>
2024-10-24 15:22:42 +0200

    fix: fix typo for label

--- react-app/frontend/src/routes/AppRoutes.tsx
+++ react-app/frontend/src/routes/AppRoutes.tsx
@@ -11,7 +11,7 @@ function AppRoutes() {
           {authRoutes.map(({ path, element }) => (
             <Route key={path} path={path} element={element} />
           ))}
-          <Route path="*" element={<Navigate to="/PortfoliosHub" />} />
+          <Route path="*" element={<Navigate to="/PortfolioHub" />} />
         </Routes>
       ) : (
         <Routes>
