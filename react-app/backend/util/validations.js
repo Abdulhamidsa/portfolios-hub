72c573ab02f2e7d74ee3d4cbe20d6920c23a9783
Abdulhamidsa <email@example.com>
2024-11-01 16:11:00 +0100

    exporting countries + adding missing required messages

--- react-app/backend/util/validations.js
+++ react-app/backend/util/validations.js
@@ -1,5 +1,5 @@
 import { z } from 'zod'
-import { preDefinedLinks, preDefinedProfessions } from '../config/user.data.config.js'
+import { preDefinedLinks, preDefinedProfessions, countriesList } from '../config/user.data.config.js'
 // upload project schema validation
 export const projectUploadSchema = z.object({
     title: z
@@ -45,19 +45,21 @@ export const signInSchema = z.object({
 // sign in schema validation
 
 export const signUpSchema = z.object({
-    firstName: z.string().min(1, 'First name is required'),
-    lastName: z.string().min(1, 'Last name is required'),
-    username: z.string().min(1, 'Username is required'),
-    email: z.string().email('Invalid email format'),
+    firstName: z.string({ required_error: 'First name is required' }),
+    lastName: z.string({ required_error: 'Last name is required' }),
+    username: z.string({ required_error: 'Username is required' }),
+    email: z.string({ required_error: 'Email is required' }).email('Invalid email format'),
     profilePicture: z.string({ required_error: 'profile picture is required' }).url('Invalid URL format'),
     // dateOfBirth: z.string().min(1, 'Date of birth is required').isOptional(),
-    password: z.string().min(6, 'Password must be at least 6 characters long'),
+    password: z
+        .string({ required_error: 'Password is required' })
+        .min(6, 'Password must be at least 6 characters long'),
     profession: z.enum(preDefinedProfessions, { message: 'Invalid profession' }),
-    country: z.string().min(1, 'Country is required'),
+    country: z.enum(countriesList, { message: 'Invalid country' }),
     links: z.array(
         z.object({
             name: z.enum(preDefinedLinks, { message: 'Invalid link name' }),
-            url: z.string().url('Invalid URL format'),
+            url: z.string({ required_error: ' Link is required' }).url('Invalid URL format'),
         })
     ),
 })
