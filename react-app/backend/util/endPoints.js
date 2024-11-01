8678d909528b09c8c473a8ed41d8ab3a709e8fb7
Abdulhamidsa <email@example.com>
2024-11-01 16:43:04 +0100

    remove extra backslash

--- react-app/backend/util/endPoints.js
+++ react-app/backend/util/endPoints.js
@@ -7,11 +7,11 @@ const path = {
 export const endPoints = {
     user: {
         auth: {
-            signin: `/${path.auth}/signin`,
-            register: `/${path.auth}/register`,
-            signout: `/${path.auth}/signout`,
-            checkAuth: `/${path.auth}/check-auth`,
-            refreshTokens: `/${path.auth}/refresh-tokens`,
+            signin: `${path.auth}/signin`,
+            register: `${path.auth}/register`,
+            signout: `${path.auth}/signout`,
+            checkAuth: `${path.auth}/check-auth`,
+            refreshTokens: `${path.auth}/refresh-tokens`,
         },
     },
 }
