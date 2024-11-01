6dc2cad31a0c541ea4327d0e3a3cce101678ca2b
Abdulhamidsa <email@example.com>
2024-11-01 16:12:15 +0100

    move hashing pass function to helper file

--- react-app/backend/util/herlper.js
+++ react-app/backend/util/herlper.js
@@ -1,5 +1,9 @@
 import { customAlphabet } from 'nanoid'
+import bcrypt from 'bcryptjs'
 export const generateFriendlyId = (firstName) => {
     const shortId = customAlphabet('1234567890abcdefg', 10)()
     return `${firstName.toLowerCase().replace(/\s/g, '-')}-${shortId}`
+}
+export const hashPassword = async (password) => {
+    return await bcrypt.hash(password, 10)
 }
