93161416a24d75e119b4f29b6a4c1de26608a114
Abdulhamidsa <email@example.com>
2024-11-05 16:54:12 +0100

    add error boundary

--- react-app/frontend/src/App.tsx
+++ react-app/frontend/src/App.tsx
@@ -2,17 +2,20 @@ import "@/App.css";
 import Layout from "@/components/Layout";
 import Loading from "@/components/feedback/Loading";
 import { AuthProvider } from "@/context/AuthContext";
+import ErrorBoundary from "@/lib/ErrorBoundary";
 import AppRoutes from "@/routes/AppRoutes";
 import { Suspense } from "react";
 
 export default function App() {
   return (
     <AuthProvider>
-      <Suspense fallback={<Loading />}>
-        <Layout>
-          <AppRoutes />
-        </Layout>
-      </Suspense>
+      <ErrorBoundary>
+        <Suspense fallback={<Loading />}>
+          <Layout>
+            <AppRoutes />
+          </Layout>
+        </Suspense>
+      </ErrorBoundary>
     </AuthProvider>
   );
 }
