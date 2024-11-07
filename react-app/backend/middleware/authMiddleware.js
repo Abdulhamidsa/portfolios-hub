353753850bdac5898a223f218d33ee28aeaf3e60
Abdulhamidsa <email@example.com>
2024-11-07 17:39:03 +0100

    fix import path

--- react-app/backend/middleware/authMiddleware.js
+++ react-app/backend/middleware/authMiddleware.js
@@ -1,8 +1,8 @@
 // import { getErrorResponse } from '../util/api.response.js'
 // import { checkAuthenticationService } from '../services/auth.service.js'
 
-import { verifyToken } from '../util/jwt.js'
-import AppError from '../util/error.handler.js'
+import { verifyToken } from '../utils/jwt.js'
+import AppError from '../utils/error.handler.js'
 export const authenticateUser = async (req, res, next) => {
     const accessToken = req.cookies.accessToken
     if (!accessToken) {
@@ -10,11 +10,9 @@ export const authenticateUser = async (req, res, next) => {
     }
     try {
         const payload = await verifyToken(accessToken, 'access')
-        // console.log('Payload:', payload)
         req.locals = { userId: payload.id }
         next()
     } catch (error) {
-        // console.error('Token verification error:', error.message)
         return next(new AppError(error.message, error.status))
     }
 }
